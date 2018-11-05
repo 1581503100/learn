113f4a6f87d64e6384d8d8c1c0a34bbed98cde19
sjliu <1581503100@qq.com>
2018-11-05 17:19:17 +0800

    complete conf

--- go/src/conf/conf.go
+++ go/src/conf/conf.go
@@ -1,48 +1,60 @@
 package conf
 
 import (
-	"os"
 	"bufio"
+	"bytes"
+	"github.com/astaxie/beego/logs"
 	"io"
-	"strings"
+	"os"
 	"strconv"
+	"strings"
 )
-var confMap map[string]string
+var confMap = map[string]string{}
+var reloadFuns []func()
+
+
 func Init(files ...string){
-	confMap=make(map[string]string)
 	for _,v:=range files{
 		parserFile(v)
 	}
 }
-
+func InitWithBytes(b []byte)  {
+	reader:=bytes.NewReader(b)
+	parser(reader)
+}
 func parserFile(file string)  {
 	f,err:=os.Open(file)
 	defer f.Close()
 	if err !=nil{
 		panic(err)
 	}
-	rd:=bufio.NewReader(f)
-	for{
-		line,err:=rd.ReadString('\n')
-		if err!=nil || io.EOF ==err{
-			break
-		}
-		if(strings.HasPrefix(line,"#")){
-			continue
-		}
-		kvs:=strings.Split(line,"=")
-		if(len(kvs)!=2){
-			continue
-		}
-		confMap[deleteSpace(kvs[0])]=deleteSpace(kvs[1])
-	}
-
+	parser(f)
 }
 
+func parser(reader io.Reader)  {
+	scaner:=bufio.NewScanner(reader)
+	for scaner.Scan(){
+		line:=scaner.Text()
+		if(strings.HasPrefix(line,"#") || len(line)==0){
+			continue
+		}
+		idx:=strings.Index(line,"=")
+		if(idx ==0 ){
+			continue
+		}
+		confMap[deleteSpace(line[0:idx])]=deleteSpace(line[idx+1:])
+	}
+
+	logs.Info("------------------------load config:",confMap)
+	for _,v:=range reloadFuns{
+		v()
+	}
+}
+//return int value of giving key
 func String(key string)string  {
 	return confMap[key]
 }
-
+//return int value of giving key and return defaultVal by default
 func Int(key string,defaultVal int) int {
 	if(len(confMap[key])==0){
 		return defaultVal
@@ -53,7 +65,7 @@ func Int(key string,defaultVal int) int {
 	}
 	return val
 }
-
+//return bool value of giving key and return defaultVal by default
 func Bool(key string,defaultVal bool) bool  {
 	if(len(confMap[key])==0){
 		return defaultVal
@@ -65,7 +77,7 @@ func Bool(key string,defaultVal bool) bool  {
 	return val
 }
 
-
+//return float64 value of giving key and return defaultVal by default
 func Float64(key string,defaultVal float64) float64  {
 	if(len(confMap[key])==0){
 		return defaultVal
@@ -76,11 +88,14 @@ func Float64(key string,defaultVal float64) float64  {
 	}
 	return val
 }
-
+//return string array value of giving key and using ',' to split each item
 func Strings(key string) []string {
+	if(confMap[key]==""){
+		return []string{}
+	}
 	return strings.Split(confMap[key],",")
 }
-
+//delete space '\n '\t' '\r' of begin and end of string
 func deleteSpace(s string) string {
 	var st,ed int
 	for i,_:=range s{
@@ -96,4 +111,8 @@ func deleteSpace(s string) string {
 		}
 	}
 	return s[st:ed]
+}
+
+func AddReloadHanler(f func()){
+	reloadFuns =append(reloadFuns,f)
 }
